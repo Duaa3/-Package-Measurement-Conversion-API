ba0de732c2337a8a7128ccd5b75795203d6e7473
Duaa3 <Duaahil44@gmail.com>
2023-05-15 21:25:37 +0400

    first commit

---
+++ API-Package-Measurement-Conversion/src/main/java/com/PackageMeasurementConversion/API/Package/Measurement/Conversion/ApiPackageMeasurementConversionApplication.java
@@ -0,0 +1,13 @@
+package com.PackageMeasurementConversion.API.Package.Measurement.Conversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPackageMeasurementConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPackageMeasurementConversionApplication.class, args);
+	}
+
+}
---
+++ API-Package-Measurement-Conversion/src/test/java/com/PackageMeasurementConversion/API/Package/Measurement/Conversion/ApiPackageMeasurementConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PackageMeasurementConversion.API.Package.Measurement.Conversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPackageMeasurementConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
